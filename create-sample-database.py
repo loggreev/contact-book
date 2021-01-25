228748bff6e079b779671fbbabc89b1861dd9a4b
loggreev <70668891+loggreev@users.noreply.github.com>
2021-01-25 11:07:47 -0500

    basic outline

---
+++ create-sample-database.py
@@ -0,0 +1,19 @@
+# this program creates a database with random sample data for the contact book
+# columns are first_name, last_name, phone_number, email
+# mock data comes from mockaroo
+
+import sqlite3 # database
+
+def main():
+    # see if database already exists
+    
+    # if it does, ask user if they want to replace it
+    
+    # load mock data file
+    
+    # create table
+    
+    # for each entry in file, insert values into table
+
+if __name__ == '__main__':
+    main()
