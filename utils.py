2f9f54f6d60ddd300e6e14283502cb0900285d1c
loggreev <70668891+loggreev@users.noreply.github.com>
2021-01-25 16:42:49 -0500

    make get_choice work with both dicts and lists

--- utils.py
+++ utils.py
@@ -1,22 +1,38 @@
 # asks the user to enter a choice from the given options
-# options is a dictionary with a key representing the displayed text and a value representing a function to call
-# returns the value of the user's choice
-def get_choice(options):
-    indexed_options = [option for option in options.keys()]
-    if len(indexed_options) < 1:
+# if options is a dictionary, a key represents the displayed text and its value is what is returned
+# if options is a list or tuple, the selected value is returned
+def get_choice(options, text = ''):
+    options_type = None
+    if type(options) is dict:
+        options_type = 'dict'
+    elif type(options) is list or type(options) is tuple:
+        options_type = 'list'
+    else:
+        raise Exception('get_choice must be used with dictionaries, lists, or tuples!')
+        
+    if len(options) < 1:
         raise Exception('get_choice must contain at least 1 option!')
+    
+    if options_type == 'dict':
+        indexed_options = [option for option in options.keys()]
+    
     # loop until a valid option is selected
     while True:
+        print(text)
         # for every given option
-        for index, option in enumerate(indexed_options):
+        for index, option in enumerate(options):
             print(f'{index+1}: {option}')
         try:
             # user enters a number corresponding to the displayed option
             choice = int(input()) - 1
-            if choice not in range(len(indexed_options)):
+            if choice not in range(len(options)):
                 raise ValueError
         except ValueError:
             print('Invalid choice.\n')
             continue
-        choice = indexed_options[choice]
-        return options[choice]
+        
+        if options_type == 'dict':
+            choice = indexed_options[choice]
+            return options[choice]
+        elif options_type == 'list':
+            return options[choice]
